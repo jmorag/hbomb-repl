349d26bce383ced49bb917bb6c7e3de1276cc024
Lauren Arnett <lba2138@columbia.edu>
2020-09-19 21:21:52 -0400

    serial number, parallel, strikes

--- src/Main.hs
+++ src/Main.hs
@@ -26,6 +26,35 @@ loop = do
         loop
       ["batteries", n] -> readBatteries n >> loop
       ["bat", n] -> readBatteries n >> loop
+      ["even"] -> do
+        lift $ modify' \s -> s {serialEven = Just True}
+        outputStrLn "Serial number is even"
+        loop
+      ["odd"] -> do
+        lift $ modify' \s -> s {serialEven = Just False}
+        outputStrLn "Serial number is odd"
+        loop
+      ["vowel"] -> do
+        lift $ modify' \s -> s {serialVowel = Just True}
+        outputStrLn "Serial number has a vowel"
+        loop
+      ["no", "vowel"] -> do
+        lift $ modify' \s -> s {serialVowel = Just False}
+        outputStrLn "Serial number does NOT have a vowel"
+        loop
+      ["parallel"] -> do
+        lift $ modify' \s -> s {parallel = Just True}
+        outputStrLn "Bomb has a parallel port"
+        loop
+      ["no", "parallel"] -> do
+        lift $ modify' \s -> s {parallel = Just False}
+        outputStrLn "Bomb does NOT have a parallel port"
+        loop
+      ["strike"] -> do
+        lift $ modify' \s -> s {strikes = (strikes s) + 1}
+        curStrikes <- lift $ gets strikes
+        outputStrLn ((show curStrikes) ++ " strikes")
+        loop
       ["wires", ws] -> case readSimpleWires ws of
         Just wires -> simpleWires wires >> loop
         Nothing -> output "Simple wires must be one of ['r' (red), 'b' (blue), 'k' (black), 'w' (white), 'y' (yellow)]" >> loop
