432b9c8e0f692186a0f46ac44abb7c983c0a0844
Lauren Arnett <lba2138@columbia.edu>
2020-09-19 20:04:51 -0400

    start repl

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,19 @@
 import RIO
+import RIO.Char
+import System.Console.Haskeline
 
-main = pure ()
+main :: IO ()
+main = runInputT defaultSettings loop
+  where
+    loop :: InputT IO ()
+    loop = do
+      minput <- getInputLine "% "
+      case minput of
+        Nothing -> return ()
+        Just "quit" -> return ()
+        Just input -> do
+          outputStrLn $ "Input was: " ++ (process input)
+          loop
+
+process :: [Char] -> [Char]
+process input = map toUpper input
