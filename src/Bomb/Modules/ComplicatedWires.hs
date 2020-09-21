a6412fed8f6fcc99ae847bb9129e7002de326c2b
Lauren Arnett <lba2138@columbia.edu>
2020-09-20 22:23:59 -0400

    readability and shadowing improvements

--- src/Bomb/Modules/ComplicatedWires.hs
+++ src/Bomb/Modules/ComplicatedWires.hs
@@ -1,7 +1,6 @@
 module Bomb.Modules.ComplicatedWires where
 
 import Bomb
-import RIO
 import RIO.Char
 import qualified RIO.Set as Set
 
@@ -31,38 +30,38 @@ readWire s = if (any (`Set.notMember` (Set.fromList ["red", "r", "blue", "b" ,"s
   in Just Wire {..}
 
 wireInstruction :: Wire -> Instruction
-wireInstruction w =
-  case w of Wire True True True True -> DoNotCut
-            Wire True True True False -> ParallelPort
-            Wire True True False True -> SerialEven
-            Wire True True False False -> SerialEven
-            Wire True False True True -> TwoOrMoreBatteries
-            Wire True False True False -> Cut
-            Wire True False False True -> TwoOrMoreBatteries
-            Wire True False False False -> SerialEven
-            Wire False True True True -> ParallelPort
-            Wire False True True False -> DoNotCut
-            Wire False True False True -> ParallelPort
-            Wire False True False False -> SerialEven
-            Wire False False True True -> TwoOrMoreBatteries
-            Wire False False True False -> Cut
-            Wire False False False True -> DoNotCut
-            Wire False False False False -> Cut
+wireInstruction = \case
+  Wire True True True True -> DoNotCut
+  Wire True True True False -> ParallelPort
+  Wire True True False True -> SerialEven
+  Wire True True False False -> SerialEven
+  Wire True False True True -> TwoOrMoreBatteries
+  Wire True False True False -> Cut
+  Wire True False False True -> TwoOrMoreBatteries
+  Wire True False False False -> SerialEven
+  Wire False True True True -> ParallelPort
+  Wire False True True False -> DoNotCut
+  Wire False True False True -> ParallelPort
+  Wire False True False False -> SerialEven
+  Wire False False True True -> TwoOrMoreBatteries
+  Wire False False True False -> Cut
+  Wire False False False True -> DoNotCut
+  Wire False False False False -> Cut
 
 
 instructionToAction :: Instruction -> Bomb ()
 instructionToAction i = do
   bats <- gets batteries
-  parallel <- gets parallel
-  even <- gets serialEven
+  parallelPort <- gets parallel
+  serialEven <- gets serialEven
   case i of Cut -> output "Cut the wire"
             DoNotCut -> output "Do NOT cut the wire"
-            ParallelPort -> case parallel of Nothing -> askParallel (output "Cut the wire") (output "Do NOT cut the wire")
+            ParallelPort -> case parallelPort of Nothing -> askParallel (output "Cut the wire") (output "Do NOT cut the wire")
+                                                 Just True -> output "Cut the wire"
+                                                 Just False -> output "Do NOT cut the wire"
+            SerialEven -> case serialEven of Nothing -> askSerialEven (output "Cut the wire") (output "Do NOT cut the wire")
                                              Just True -> output "Cut the wire"
                                              Just False -> output "Do NOT cut the wire"
-            SerialEven -> case even of Nothing -> askSerialEven (output "Cut the wire") (output "Do NOT cut the wire")
-                                       Just True -> output "Cut the wire"
-                                       Just False -> output "Do NOT cut the wire"
             TwoOrMoreBatteries -> case bats of Nothing -> askBatteries \case
                                                  MoreThanTwo -> output "Cut the wire"
                                                  Two -> output "Cut the wire"
